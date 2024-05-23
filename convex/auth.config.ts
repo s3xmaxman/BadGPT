9d5278c95b1a3fe5adf740875289365c06e76a1e
s3xmaxman <badside3919@gmail.com>
2024-05-23 16:19:58 +0900

    setUp

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://national-magpie-44.clerk.accounts.dev/",
+            applicationID: "convex",
+        },
+    ]
+};
---
+++ provider/convex-client-provider.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { Loading } from "@/components/auth/loading";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { AuthLoading, Authenticated, ConvexReactClient, Unauthenticated } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+
+interface ConvexClientProviderProps {
+    children: React.ReactNode;
+}
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL
+
+if (!convexUrl) {
+    throw new Error("NEXT_PUBLIC_CONVEX_URL is not defined");
+}
+
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider = ({ children }: ConvexClientProviderProps) => {
+    return (
+        <ClerkProvider afterSignUpUrl="/sign-up">
+        <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+            <AuthLoading>
+                <Loading />
+            </AuthLoading>
+            <Authenticated>
+                {children}
+            </Authenticated>
+            <Unauthenticated>
+                {children}
+            </Unauthenticated>
+        </ConvexProviderWithClerk>
+    </ClerkProvider>
+    );
+}
