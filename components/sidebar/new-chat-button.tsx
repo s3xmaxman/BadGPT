58ba5158e49bd2a7d013061ccd0ced7d2f5607d1
s3xmaxman <badside3919@gmail.com>
2024-05-23 18:03:00 +0900

    shadcn setup

--- components/sidebar/new-chat-button.tsx
+++ components/sidebar/new-chat-button.tsx
@@ -1,7 +1,14 @@
+import { PlusCircle, SquarePen } from "lucide-react"
+import { Button } from "../ui/button"
+
 export const NewChatButton = () => {
-    return (
-        <div>
-            NewChatButton
-        </div>
-    )
+  return (
+    <Button
+      className="w-full flex justify-start items-center bg-inherit hover:bg-inherit p-0"
+    >
+        <PlusCircle className="w-5 h-5" />
+        <p className="font-semibold text-start ml-3">新しいチャット</p>
+        <SquarePen className="w-4 h-4 ml-auto" />
+    </Button>
+  )
 }
